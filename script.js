ff904e8e4ada950c6970e7603360c7b7e0884c0e
mr0-0kushal <kushalsharmahacker678@gmail.com>
2024-02-13 17:49:29 +0530

    8th Commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let CreateAccContent = '<div class="form1"><h2>SignUp</h2><h6>Stay updated on your chit chats</h6><form method="POST" class="signup"><input type="text" id="usrname" placeholder="Username" autofocus ><input type="email" id="email" placeholder="Email"><br><input type="password" id="passw" placeholder="Create Password" required><br><input type="password" id="passw" placeholder="Confirm Password" required><br><input type="number" placeholder = "Enter 10 Digit Phone Number"  id="phone"><br><input type="submit" value="Create" class="sign"><span class="shows" onclick= "shows()" >show</span><span class="login" onclick="LoginPage()">Already have an Account?</span></form></div>';
+let CreateAccContent = '<div class="form1"><h2>SignUp</h2><h6>Stay updated on your chit chats</h6><form method="POST" class="signup"><input type="text" id="usrname" placeholder="Username" autofocus ><input type="email" id="email" placeholder="Email"><br><input type="password" id="passw" placeholder="Create Password" required><br><input type="password" id="passwr" placeholder="Confirm Password" required><br><input type="number" placeholder = "Enter 10 Digit Phone Number"  id="phone"><br><input type="submit" value="Create" class="sign"><span class="shows" onclick= "shows()" >show password</span><span class="logins" onclick="LoginPage()">Already have an Account?</span></form></div>';
 let loginContent = '<div class="form"><h2>Login</h2><h6>Stay updated on your chit chats</h6><form method="POST" name="login" class="login"><input type="text" id="usr" placeholder="Username or Email" autofocus ><br><input type="password" id="pass" placeholder="Password" required><br><input type="submit" value="LOGIN" class="submit"><span class="show" onclick= "show()" >show password</span><span class="createacc" onclick="createacc()">Don\'t have an Account?</span></form></div>';
 let main = document.querySelector('.dash-main')
 let loginBtn = document.querySelector('.logBtn')
@@ -29,13 +29,21 @@ function show(){
     }
 }
 function shows(){
-    var input = document.getElementById("passw");
+    var input1 = document.getElementById("passw");
+    if (input1.type === "password"){
+        input1.type = "text";
+    }
+    else {
+        input1.type="password";
+    }
+    var input = document.getElementById("passwr");
     if (input.type === "password"){
         input.type = "text";
     }
     else {
         input.type="password";
     }
+
 }
 // create account button js-----
 function createacc(){
