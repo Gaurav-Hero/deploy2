8d50df2c5478194a323525763ae1edf165e71d4b
mr0-0kushal <kushalsharmahacker678@gmail.com>
2024-02-13 12:33:04 +0530

    seventh commit

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
+let CreateAccContent = '<div class="form1"><h2>SignUp</h2><h6>Stay updated on your chit chats</h6><form method="POST" class="signup"><input type="text" id="usrname" placeholder="Username" autofocus ><input type="email" id="email" placeholder="Email"><br><input type="password" id="passw" placeholder="Create Password" required><br><input type="password" id="passw" placeholder="Confirm Password" required><br><input type="number" placeholder = "Enter 10 Digit Phone Number"  id="phone"><br><input type="submit" value="Create" class="sign"><span class="shows" onclick= "shows()" >show</span><span class="login" onclick="LoginPage()">Already have an Account?</span></form></div>';
+let loginContent = '<div class="form"><h2>Login</h2><h6>Stay updated on your chit chats</h6><form method="POST" name="login" class="login"><input type="text" id="usr" placeholder="Username or Email" autofocus ><br><input type="password" id="pass" placeholder="Password" required><br><input type="submit" value="LOGIN" class="submit"><span class="show" onclick= "show()" >show password</span><span class="createacc" onclick="createacc()">Don\'t have an Account?</span></form></div>';
 let main = document.querySelector('.dash-main')
 let loginBtn = document.querySelector('.logBtn')
-loginBtn.addEventListener('click',function(){
-    main.innerHTML ='<div class="form"><h2>Login</h2><h6>Stay updated on your chit chats</h6><form method="POST" name="login" class="login"><input type="text" name="usr" id="usr" placeholder="Username or Email" autofocus ><br><input type="password" name="pass" id="pass" placeholder="Password" required><br><input type="submit" value="LOGIN" class="submit"><span class="show" onclick= "show()" >show</span><span class="createacc" onclick="createacc()">Don\'t have an Account?</span></form></div>';
-})
+loginBtn.addEventListener('click',LoginPage())
 let text = document.querySelector('#nav-part2')
 let body  = document.querySelector('#page1');
 let flag = 0;
@@ -28,8 +28,20 @@ function show(){
         input.type="password";
     }
 }
+function shows(){
+    var input = document.getElementById("passw");
+    if (input.type === "password"){
+        input.type = "text";
+    }
+    else {
+        input.type="password";
+    }
+}
 // create account button js-----
 function createacc(){
-    main.innerHTML='HI';
-    console.log("clicked");  
+    main.innerHTML=CreateAccContent;
+    console.log("clicked"); 
+}
+function LoginPage(){
+    main.innerHTML =loginContent;
 }
